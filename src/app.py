37d41081fc0e6ffead8b58cd01df342be4e7c08a
marekpiotrowski <marek_piotrowski@live.com>
2020-08-31 20:44:43 +0200

    Added required arguments

--- src/app.py
+++ src/app.py
@@ -3,8 +3,8 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--doc", help="Input document for processing.")
-    parser.add_argument("--project-dir", help="Absolute project directory.")
+    parser.add_argument("--doc", help="Input document for processing.", required=True)
+    parser.add_argument("--project-dir", help="Absolute project directory.", required=True)
     args = parser.parse_args()
     input_document_path = args.doc
     project_root = args.project_dir
