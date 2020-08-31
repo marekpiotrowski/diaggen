ad137c91548fdf19453dc0a4968892fb45ff501d
marekpiotrowski <marek_piotrowski@live.com>
2020-08-31 20:32:32 +0200

    Extended parsing and context-dir flattening

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--doc", help="Input document for processing.")
+    parser.add_argument("--project-dir", help="Absolute project directory.")
     args = parser.parse_args()
     input_document_path = args.doc
-    static_generator = StaticGenerator(input_document_path)
+    project_root = args.project_dir
+    static_generator = StaticGenerator(input_document_path, project_root)
--- src/static_generator/static_generator.py
+++ src/static_generator/static_generator.py
@@ -1,15 +1,26 @@
 import argparse
+import os
 
 
 class GeneratorCommand(object):
+    project_root = "/" # TODO refactor this one
+
     def __init__(self, diagram_identifier, context, context_dir):
+        if (context is None and context_dir is None) or (context is not None and context_dir is not None):
+            raise Exception("Incorrect static generator command. You can't provide both ctx and ctx-dir.")
+        if context_dir is not None:
+            context = self.__expand_context_dir(context_dir)
         self.diagram_identifier = diagram_identifier
         self.context = context
-        self.context_dir = context_dir
+
+    def __expand_context_dir(self, context_dir):
+        files = os.listdir(os.path.join(self.project_root, context_dir))
+        files = list(map(lambda f: os.path.join(context_dir, f), files))
+        return ','.join(files)
 
     def __str__(self):
-        return "Diagram identifier: {}, context: {}, context directory: {}".format(
-            self.diagram_identifier, self.context, self.context_dir)
+        return "Diagram identifier: {}, context: {}".format(
+            self.diagram_identifier, self.context)
 
 
 class StaticGenerator(object):
@@ -17,19 +28,26 @@ class StaticGenerator(object):
     OPENING_COMMENT_TOKEN = "<!--"
     CLOSING_COMMENT_TOKEN = "-->"
 
-    def __init__(self, input_document_abs_path):
-        print(input_document_abs_path)
-        self.__input_document_abs_path = input_document_abs_path
+    def __init__(self, input_document_path, project_root):
+        print(input_document_path)
+        GeneratorCommand.project_root = project_root # TODO refactor this one, yet we don't want to set it on each instance, right?
+        self.__input_document_abs_path = input_document_path
         self.__find_static_generator_cmds()
 
     def __find_static_generator_cmds(self):
         with open(self.__input_document_abs_path) as docfile:
             line = docfile.readline()
+            line_number = 1
             while line:
                 if self.STATIC_DIAGGEN_TOKEN in line:
-                    cmd = self.__parse_static_generator_cmd(line)
-                    print(cmd)
+                    try:
+                        cmd = self.__parse_static_generator_cmd(line)
+                        print(cmd)
+                    except Exception as e:
+                        print("Error in file {} at line {}.".format(self.__input_document_abs_path, line_number))
+                        print(e)
                 line = docfile.readline()
+                line_number = line_number + 1
 
     def __parse_static_generator_cmd(self, line):
         line = line.replace(self.OPENING_COMMENT_TOKEN, ''). \
