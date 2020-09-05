588e60dda81d310589cfa546db42c86943e01fb5
mpiotrowski <marek_piotrowski@live.com>
2020-09-05 10:34:02 +0200

    State is persisted in class, not within a function - translation unit parser

--- src/static_generator/cpp_translation_unit_extractor.py
+++ src/static_generator/cpp_translation_unit_extractor.py
@@ -7,128 +7,61 @@ class CppTranslationUnitExtractor(object):
     def __init__(self, abs_filepath, abs_includes):
         self.__translation_unit_abs_filepath = abs_filepath
         self.__abs_includes = abs_includes
+        self.__context = None
+        self.__registered_classes = {}
+        index = clang.cindex.Index.create()
+        clang_args = ['-std=c++14'] # TODO do something with args...
+        includes = ["-I" + include for include in self.__abs_includes]
+        clang_args = clang_args + includes
+        self.__translation_unit = index.parse(self.__translation_unit_abs_filepath,
+                                       args=clang_args)
 
     def get_classes(self):
-        result = self.__traverse_top_to_bottom()
-        return list(result.values())
+        self.__context = None
+        self.__registered_classes = {}
+        self.__traverse_top_to_bottom()
+        return list(self.__registered_classes.values())
 
     @staticmethod
     def __is_exposed_field(node):
         return node.access_specifier == clang.cindex.AccessSpecifier.PUBLIC
 
+    def __add_class_if_not_registered(self, node):
+        if node is not None and node.kind in [clang.cindex.CursorKind.CLASS_DECL,
+                                              clang.cindex.CursorKind.STRUCT_DECL] and \
+                node.spelling not in self.__registered_classes:
+            self.__registered_classes[node.spelling] = ClassMetadata(name=node.spelling, methods=[], parents=[])
+
+    def __add_method_if_not_registered(self, new_node):
+        if self.__context is None or new_node.kind != clang.cindex.CursorKind.CXX_METHOD or not self.__is_exposed_field(new_node):
+            return
+        if self.__context.spelling not in self.__registered_classes:
+            return
+        if self.__registered_classes[self.__context.spelling].has_method(new_node.spelling):
+            return
+        arguments = [a.type.spelling for a in new_node.get_arguments()]
+        method = MethodMetadata(name=new_node.spelling, arguments=arguments, return_type=new_node.result_type.spelling)
+        self.__registered_classes[self.__context.spelling].add_method(method)
+
+    def __add_parent_if_not_registered(self, new_node):
+        if self.__context is None:
+            return
+        if new_node.kind != clang.cindex.CursorKind.CXX_BASE_SPECIFIER:
+            return
+        parent_name = new_node.spelling.split('::')[-1] # for some reason, parent base specifier has full namespace information
+        self.__registered_classes[self.__context.spelling].add_parent(parent_name)
+
     def __traverse_top_to_bottom(self):
-        index = clang.cindex.Index.create()
-        clang_args = ['-std=c++14'] # TODO do something with args...
-        includes = ["-I" + include for include in self.__abs_includes]
-        clang_args = clang_args + includes
-        translation_unit = index.parse(self.__translation_unit_abs_filepath,
-                                       args=clang_args)
-        context = {'$': None} # via dictionary, we need a mutable object
-        registered_classes = {}
-
-        def add_class_if_not_registered(node):
-            if node is not None and node.kind in [clang.cindex.CursorKind.CLASS_DECL, clang.cindex.CursorKind.STRUCT_DECL] and \
-                    node.spelling not in registered_classes:
-                registered_classes[node.spelling] = ClassMetadata(name=node.spelling, methods=[], parents=[])
-
-        def add_method_if_not_registered(new_node):
-            if context['$'] is None or new_node.kind != clang.cindex.CursorKind.CXX_METHOD or not self.__is_exposed_field(new_node):
-                return
-            if context['$'].spelling not in registered_classes:
-                return
-            if registered_classes[context['$'].spelling].has_method(new_node.spelling):
-                return
-            arguments = [a.type.spelling for a in new_node.get_arguments()]
-            method = MethodMetadata(name=new_node.spelling, arguments=arguments, return_type=new_node.result_type.spelling)
-            registered_classes[context['$'].spelling].add_method(method)
-
-        def add_parent_if_not_registered(new_node):
-            if context['$'] is None:
-                return
-            if new_node.kind != clang.cindex.CursorKind.CXX_BASE_SPECIFIER:
-                return
-            parent_name = new_node.spelling.split('::')[-1] # for some reason, parent base specifier has full namespace information
-            registered_classes[context['$'].spelling].add_parent(parent_name)
-
         def classify(node):
             if node.kind == clang.cindex.CursorKind.CLASS_DECL:
-                context['$'] = node
-            add_class_if_not_registered(context['$'])
-            add_method_if_not_registered(node)
-            add_parent_if_not_registered(node)
+                self.__context = node
+            self.__add_class_if_not_registered(self.__context)
+            self.__add_method_if_not_registered(node)
+            self.__add_parent_if_not_registered(node)
 
         def visit(node, func):
             func(node)
             for c in node.get_children():
                 visit(c, func)
 
-        visit(translation_unit.cursor, classify)
-        return registered_classes
-
-
-
-        # base_specifiers = self.__filter_node_list_by_node_kind(translation_unit.cursor.get_children(),
-        #                                                        [clang.cindex.CursorKind.CXX_BASE_SPECIFIER],
-        #                                                        go_into=[clang.cindex.CursorKind.CLASS_DECL,
-        #                                                                 clang.cindex.CursorKind.STRUCT_DECL,
-        #                                                                 clang.cindex.CursorKind.NAMESPACE])
-        # for c in base_specifiers:
-        #     # if c.kind == clang.cindex.CursorKind.CXX_BASE_SPECIFIER:
-        #     print(c.spelling)
-
-# def is_exposed_field(node):
-#     return node.access_specifier == clang.cindex.AccessSpecifier.PUBLIC
-#
-#
-# def find_all_exposed_fields(
-#         cursor: clang.cindex.Cursor
-# ):
-#     result = []
-#     field_declarations = filter_node_list_by_node_kind(cursor.get_children(), [clang.cindex.CursorKind.CXX_METHOD])
-#     for i in field_declarations:
-#         if not is_exposed_field(i):
-#             continue
-#         result.append(i.displayname)
-#     return result
-#
-#
-# def filter_node_list_by_node_kind(
-#         nodes: typing.Iterable[clang.cindex.Cursor],
-#         kinds: list
-# ) -> typing.Iterable[clang.cindex.Cursor]:
-#     result = []
-#     for i in nodes:
-#         # print(i.kind)
-#         if i.kind == clang.cindex.CursorKind.NAMESPACE:
-#             return filter_node_list_by_node_kind(i.get_children(), [clang.cindex.CursorKind.CLASS_DECL,
-#                                                                     clang.cindex.CursorKind.STRUCT_DECL])
-#         if i.kind in kinds:
-#             result.append(i)
-#     return result
-#     # for i in nodes:
-#     #     print(list(i.get_children()))
-#     #     if i.kind in kinds:
-#     #         result.append(i)
-#
-#
-# all_classes = filter_node_list_by_node_kind(translation_unit.cursor.get_children(),
-#                                             [clang.cindex.CursorKind.CLASS_DECL, clang.cindex.CursorKind.STRUCT_DECL])
-# for i in all_classes:
-#     print(i.spelling)
-#     print(find_all_exposed_fields(i))
-
-# lil helper!
-# import clang.cindex
-#
-# index = clang.cindex.Index.create()
-# tu = index.parse(sys.argv[1], args=["-std=c++98"], options=clang.cindex.TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
-#
-# def printall_visitor(node):
-#     print 'Found grammar element "%s" {%s} [line=%s, col=%s]' % (node.displayname, node.kind, node.location.line, node.location.column)
-#
-# def visit(node, func):
-#     func(node)
-#     for c in node.get_children():
-#         visit(c, func)
-#
-# visit(tu.cursor, printall_visitor)
+        visit(self.__translation_unit.cursor, classify)
