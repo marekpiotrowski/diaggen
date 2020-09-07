61440abe09cf7f497bba8387cf5437aace5081a6
mpiotrowski <marek_piotrowski@live.com>
2020-09-07 10:00:42 +0200

    Added arguments printing and line breaks

--- src/static_generator/puml/static_puml_formatter.py
+++ src/static_generator/puml/static_puml_formatter.py
@@ -5,12 +5,24 @@ class StaticPumlFormatter(object):
     def __init__(self):
         pass
 
+    @staticmethod
+    def break_too_long_lines(lines):
+        # todo naively break; later on, we have to detect whitespaces
+        BREAK_AT = 60
+        result_with_breaks = ""
+        for i in range(len(lines.splitlines())):
+            line = lines.splitlines()[i]
+            line_with_breaks = '\\n'.join(line[i:i + BREAK_AT] for i in range(0, len(line), BREAK_AT))
+            result_with_breaks = result_with_breaks + line_with_breaks + "\n"
+        return result_with_breaks
+
     def get_puml_for_model(self, classes, relations):
         result = '@startuml\n'
         for c in classes:
             result = result + "class {} {{\n".format(c.name)
             for m in c.methods:
-                result = result + "    +{} {}()\n".format(m.return_type_name, m.name) # todo add arguments!
+                args = ", ".join(m.arguments)
+                result = result + "    +{} {}({})\n".format(m.return_type_name, m.name, args) # todo add arguments!
             result = result + "}\n"
         for r in relations:
             relation_as_uml = ""
@@ -20,4 +32,5 @@ class StaticPumlFormatter(object):
                 relation_as_uml = "{} <|-- {}\n".format(r.party_b, r.party_a)
             result = result + relation_as_uml
         result = result + "@enduml"
+        result = self.break_too_long_lines(result)
         return result
