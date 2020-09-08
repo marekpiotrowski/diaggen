17aa670ee228703d4a1ccfa3f8e496707fdb71d2
mpiotrowski <marek_piotrowski@live.com>
2020-09-08 12:54:43 +0200

    Added initial regex matching umls

--- src/common/md_with_puml_to_pdf_generator.py
+++ src/common/md_with_puml_to_pdf_generator.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 class MdWithPumlToPdfGenerator(object):
     def __init__(self):
@@ -6,8 +7,16 @@ class MdWithPumlToPdfGenerator(object):
 
     def generate(self, md_file_path):
         temporary_dir_for_files = os.path.dirname(md_file_path)
+        with open(md_file_path, 'r') as md_file:
+            content = md_file.read()
+        self.__convert_puml_to_images(content)
         print(temporary_dir_for_files)
 
-    def __convert_puml_to_images(self):
+    @staticmethod
+    def __convert_puml_to_images(md_file_contents):
         # right now, let's traverse whole file and extract pumls. Later on, we shall optimize it.
-        pass
+        # todo fix regex
+        puml_regexp = re.compile(r"```(.*)+```$", re.MULTILINE)
+        # right now, let's traverse whole file and extract pumls. Later on, we shall optimize it.
+        umls = puml_regexp.findall(md_file_contents)
+        print(umls)
