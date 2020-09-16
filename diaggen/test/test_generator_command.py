db9208967d6737d186bef3cbd1e0c10dcad020d8
mpiotrowski <marek_piotrowski@live.com>
2020-09-16 09:32:57 +0200

    Added important test for generator command module

--- diaggen/test/test_generator_command.py
+++ diaggen/test/test_generator_command.py
@@ -4,12 +4,18 @@ import shutil
 
 from ..src.static_generator.generator_command import GeneratorCommand
 from ..src.static_generator.config import Config
+from ..src.static_generator.model.class_metadata import ClassMetadata
+from ..src.static_generator.model.relation import Relation
 
 
 class DummyTranslationUnitExtractor(object):
     translation_units = []
     abs_includes = []
     ctor_call_count = 0
+    get_classes_call_count = 0
+    demangle_relations_call_count = 0
+    demangle_relations_result = []
+    get_classes_result = []
 
     def __init__(self, translation_unit, abs_includes):
         DummyTranslationUnitExtractor.translation_units.append(translation_unit)
@@ -17,11 +23,34 @@ class DummyTranslationUnitExtractor(object):
         DummyTranslationUnitExtractor.ctor_call_count = DummyTranslationUnitExtractor.ctor_call_count + 1
 
     def get_classes(self):
-        return []
+        DummyTranslationUnitExtractor.get_classes_call_count = DummyTranslationUnitExtractor.get_classes_call_count + 1
+        return DummyTranslationUnitExtractor.get_classes_result
 
     @staticmethod
     def demangle_relations(classes):
-        return []
+        DummyTranslationUnitExtractor.demangle_relations_call_count = DummyTranslationUnitExtractor.demangle_relations_call_count + 1
+        return DummyTranslationUnitExtractor.demangle_relations_result
+
+    @staticmethod
+    def set_demangle_relations_result(result):
+        DummyTranslationUnitExtractor.demangle_relations_result = result
+
+    @staticmethod
+    def set_get_classes_result(result):
+        DummyTranslationUnitExtractor.get_classes_result = result
+
+    @staticmethod
+    def reset():
+        DummyTranslationUnitExtractor.translation_units = []
+        DummyTranslationUnitExtractor.abs_includes = []
+        DummyTranslationUnitExtractor.ctor_call_count = 0
+        DummyTranslationUnitExtractor.get_classes_call_count = 0
+        DummyTranslationUnitExtractor.demangle_relations_call_count = 0
+
+
+@pytest.fixture(autouse=True)
+def reset_dummies():
+    DummyTranslationUnitExtractor.reset()
 
 @pytest.fixture()
 def create_ctx_dir():
@@ -66,3 +95,27 @@ def test_generator_command_ctx_dir_expands_correctly(create_ctx_dir):
     assert model.relations == []
     assert h1 in DummyTranslationUnitExtractor.translation_units and h2 in DummyTranslationUnitExtractor.translation_units
     assert DummyTranslationUnitExtractor.ctor_call_count == 2
+    assert DummyTranslationUnitExtractor.get_classes_call_count == 2
+    assert DummyTranslationUnitExtractor.demangle_relations_call_count == 1
+
+
+def test_generator_command_assembles_model_correctly(create_ctx_dir):
+    # given
+    Config.set_class_metadata_extractor(DummyTranslationUnitExtractor)
+    classes_result = [ClassMetadata("Class1", [], parents=[]), ClassMetadata("Class2", [], parents=[])]
+    relations_result = [Relation("Class1", "Class2", Relation.INHERITANCE)]
+    DummyTranslationUnitExtractor.set_demangle_relations_result(relations_result)
+    DummyTranslationUnitExtractor.set_get_classes_result(classes_result)
+    GeneratorCommand.project_root = os.getcwd()
+
+    # when
+    cmd, error = GeneratorCommand.try_parse("<!-- @diaggen-static@ --id=overall --ctx=example_ctx_dir/header1.h -->")
+    assert error is None
+    model = cmd.get_model()
+
+    # then
+    assert model.classes == classes_result
+    assert model.relations == relations_result
+    assert DummyTranslationUnitExtractor.ctor_call_count == 1
+    assert DummyTranslationUnitExtractor.get_classes_call_count == 1
+    assert DummyTranslationUnitExtractor.demangle_relations_call_count == 1
