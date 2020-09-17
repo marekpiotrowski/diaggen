ca810ebf4e392e2f2f3b6e694292bc636b1722cc
mpiotrowski <marek_piotrowski@live.com>
2020-09-17 19:22:53 +0200

    Commented playing with mocks - does not work yet

--- diaggen/test/test_static_generator.py
+++ diaggen/test/test_static_generator.py
@@ -4,7 +4,7 @@ import shutil
 
 from ..src.static_generator.static_generator import StaticGenerator
 
-# def test_static_generator_input_doc_does_not_exist():
-#     # TODO expect exception with some string!
-#     static_generator = StaticGenerator("/home", "nonexisting.md.in")
-#     static_generator.expand_static_generator_cmds()
+def test_static_generator_input_doc_does_not_exist():
+    # TODO expect exception with some string!
+    static_generator = StaticGenerator("/home", "nonexisting.md.in")
+    static_generator.expand_static_generator_cmds()
